7f1b19bb650413b65f1956f7fb7ba173a2c36707
carolineeghisolfi <ceg1998@alumni.stanford.edu>
2025-02-28 02:12:53 -0600

    clean args

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException
 import pandas as pd
 
 ## GLOBAL VARS
-id_cols = ['acct', 'owner_2024', 'address_2024', 'city_2024', 'zip_2024', 'tot_mkt_val_2024', 'tot_mkt_val_2023', 
+id_cols = ['acct', 'owner_2024', 'address_2024', 'city_2024', 'zip_2024', 'tot_mkt_val_2024', 'tot_mkt_val_2023', 'new_construction_val_2024',
            'yr_built_2024', 'yr_remodel_2024', 'bed_2024', 'bed_2023', 'bath_2024', 'bath_2023', 
            'stories_2024', 'stories_2023']
 group_cols = ['tot_mkt_val_2023_bin', 'neighborhood_grp_2024', 'neighborhood_grp_2023', 
@@ -28,7 +28,7 @@ def get_address_data(address: str, city: str, zip: int) -> Dict[str, Any]:
     data = {
         "About this home": acct_info[id_cols].to_dict(orient="records")[0],
         "Matching criteria (group 1)": acct_info[["group1_id"] + group_cols].to_dict(orient="records")[0],
-        "Result (group 1)": acct_info[['tot_mkt_val_pctchange', 'group1_typical_mkt_val_pctchange']].to_dict(orient="records")[0],
+        "Result (group 1)": acct_info[['tot_mkt_val_pctchange', 'tot_mkt_val_pctchange_adj', 'group1_typical_mkt_val_pctchange']].to_dict(orient="records")[0],
         "Matches (group 1)": master[master["group1_id"] == acct_info["group1_id"].iloc[0]][id_cols].to_dict(orient="records"),
     }
 
