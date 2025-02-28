6be7fb4af8d18f67bc52b508c626a34de9f8c29d
carolineeghisolfi <ceg1998@alumni.stanford.edu>
2025-02-28 01:58:18 -0600

    speed up loading time

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException
 import pandas as pd
 
 ## GLOBAL VARS
-id_cols = ['acct', 'owner_2024', 'address_2024', 'city_2024', 'tot_mkt_val_2024', 'tot_mkt_val_2023', 
+id_cols = ['acct', 'owner_2024', 'address_2024', 'city_2024', 'zip_2024', 'tot_mkt_val_2024', 'tot_mkt_val_2023', 
            'yr_built_2024', 'yr_remodel_2024', 'bed_2024', 'bed_2023', 'bath_2024', 'bath_2023', 
            'stories_2024', 'stories_2023']
 group_cols = ['tot_mkt_val_2023_bin', 'neighborhood_grp_2024', 'neighborhood_grp_2023', 
@@ -12,18 +12,18 @@ group_cols = ['tot_mkt_val_2023_bin', 'neighborhood_grp_2024', 'neighborhood_grp
               'stories_2024_bin', 'stories_2023_bin']
 
 app = FastAPI()
-master = pd.read_parquet('https://sfc-project-files.s3.amazonaws.com/tx-data/hc-property-taxes/master.gzip')
 
-def get_address_data(master, address: str, city: str) -> Dict[str, Any]:
+def get_address_data(address: str, city: str, zip: int) -> Dict[str, Any]:
     """Fetches address-related property data."""
 
     address = address.upper()
     city = city.upper()
 
+    master = pd.read_parquet(f'https://sfc-project-files.s3.amazonaws.com/tx-data/hc-property-taxes/master_{zip}.gzip')
     acct_info = master[(master["address_2024"] == address) & (master["city_2024"] == city)]
 
     if acct_info.empty:
-        raise HTTPException(status_code=404, detail="No data found for the given address and city.")
+        raise HTTPException(status_code=404, detail="No data found for the given address.")
 
     data = {
         "About this home": acct_info[id_cols].to_dict(orient="records")[0],
@@ -39,6 +39,6 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/get_address_data")
-def fetch_address_data(address: str, city: str):
+def fetch_address_data(address: str, city: str, zip: int):
     """API endpoint to retrieve property data by address and city."""
-    return get_address_data(master, address, city)
+    return get_address_data(address, city, zip)
